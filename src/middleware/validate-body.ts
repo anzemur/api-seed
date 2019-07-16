e4ad8dd64dcf77f4c6c48b09b942b129dda1cf9a
Anze Mur <am3158@student.uni-lj.si>
2019-07-17 00:27:50 +0200

    Added validate body middleware.

---
+++ src/middleware/validate-body.ts
@@ -0,0 +1,26 @@
+import { RequestHandler } from 'express';
+import { AuthRequest, AuthResponse } from './authentication';
+import { NextFunction } from 'connect';
+import { isRequestBodyEmpty } from '../lib/validators';
+import { BadRequestError, ValidationError } from '../lib/errors';
+import * as Joi from 'joi';
+
+/**
+ * Validates request body with provided schema.
+ * @param bodySchema Joi schema validation object.
+ */
+export function validateBody(bodySchema: Joi.ObjectSchema): RequestHandler {
+  return async (req: AuthRequest, res: AuthResponse, next: NextFunction) => {
+    const body = req.body;
+
+    if (isRequestBodyEmpty(body)) {
+      return next(new BadRequestError('Request body is empty.'));
+    }
+
+    const { error } = Joi.validate(body, bodySchema, { abortEarly: false });
+    if (error) {
+      return next(new ValidationError('Request body validation failed.', error));
+    }
+    next();
+  };
+}

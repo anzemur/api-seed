6e8593acea46e433f1b8588fb6a4226d0d8a3dc1
Anze Mur <am3158@student.uni-lj.si>
2019-08-22 02:28:01 +0200

    Added user roles modal.

---
+++ src/admin-console/components/UserRolesModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-modal id="user-roles-modal" title="Manage user roles">
+    </b-modal>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    roles: {}
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import '~/assets/scss/styles';
+</style>
